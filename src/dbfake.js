c08b3b918b54e022a3b1b240ba12f65009632934
Gabriel Judá <90941036+Gabrieljuda1@users.noreply.github.com>
2021-11-27 13:58:14 -0300

    add src

---
+++ src/dbfake.js
@@ -0,0 +1,13 @@
+var db = {
+    dados: [
+        {
+            Titulo: 'Evento'
+            Categoria: 'Show do Alok'
+        },{
+            Titulo: 'Evento'
+            Categoria: 'jogo do Atletico MG'
+        },{
+            Titulo: 'Evento'
+        Categoria: 'Palestra de negócios'}
+    ]
+}
